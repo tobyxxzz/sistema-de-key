8b5252100f15d0f52c1374065f4c845435e55913
tobyxxzz <pedrolulapedra@gmail.com>
2025-12-26 14:25:42 -0300

    Update key.js

--- api/key.js
+++ api/key.js
@@ -1,14 +1,11 @@
-// api/key.js
 export default function handler(req, res) {
+  console.log("Referer recebido:", req.headers.referer);
   const referer = req.headers.referer || "";
 
-  // só libera se veio do Work Ink
   if (!referer.startsWith("https://work.ink/")) {
     return res.status(403).json({ error: "Access Denied" });
   }
 
-  // key do dia (pode trocar todo dia ou gerar dinamicamente)
   const keyDoDia = "KEY1234"; 
-
   res.status(200).json({ key: keyDoDia });
 }
