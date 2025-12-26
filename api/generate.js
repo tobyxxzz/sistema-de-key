cc6d746cb4f21b4fa439968620b0cb189cd2d73c
tobyxxzz <pedrolulapedra@gmail.com>
2025-12-26 17:44:17 -0300

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -1,19 +1,9 @@
-import crypto from "crypto";
-
-const SECRET = "ZENITHGOD"; // troca isso
-
 export default function handler(req, res) {
-  const time = Date.now();
-  const payload = `${time}`;
-  const sig = crypto
-    .createHmac("sha256", SECRET)
-    .update(payload)
-    .digest("hex")
-    .slice(0, 8);
+  const token = Math.random().toString(36).slice(2, 10)
 
-  const token = `${time}.${sig}`;
-
-res.json({
-  url: "/k/?token=ABC123"
-});
-
+  res.setHeader("Cache-Control", "no-store")
+  res.writeHead(302, {
+    Location: `/k/?token=${token}`
+  })
+  res.end()
+}
