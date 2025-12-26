4f76cf426198730be1931d81ac1fd3d50b6b1b40
tobyxxzz <pedrolulapedra@gmail.com>
2025-12-26 14:27:40 -0300

    Update key.js

--- api/key.js
+++ api/key.js
@@ -2,7 +2,7 @@ export default function handler(req, res) {
   console.log("Referer recebido:", req.headers.referer);
   const referer = req.headers.referer || "";
 
-  if (!referer.startsWith("https://work.ink/")) {
+  if (!referer.startsWith("https://work.ink/2bAf/sistema-de-key")) {
     return res.status(403).json({ error: "Access Denied" });
   }
 
