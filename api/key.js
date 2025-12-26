6003f8efdb544012d6071d33f2493a73fc0d1bfc
tobyxxzz <pedrolulapedra@gmail.com>
2025-12-26 14:31:09 -0300

    Update key.js

--- api/key.js
+++ api/key.js
@@ -1,11 +1,10 @@
 export default function handler(req, res) {
-  console.log("Referer recebido:", req.headers.referer);
   const referer = req.headers.referer || "";
 
   if (!referer.startsWith("https://work.ink/2bAf/sistema-de-key")) {
-    return res.status(403).json({ error: "Access Denied" });
+    return res.status(403).json({ error: "Acesso bloqueado! Use o Work Ink." });
   }
 
-  const keyDoDia = "KEY1234"; 
+  const keyDoDia = "KEY1234"; // aqui tu pode colocar a key do dia
   res.status(200).json({ key: keyDoDia });
 }
